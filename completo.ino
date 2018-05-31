6f59638cb3b0a50bcb3ba1ae9849823df4e1c342
GOESTH <adiaztorres590@gmail.com>
2018-05-31 15:55:02 -0500

    Brazo robot toma un objeto

---
+++ completo.ino
@@ -0,0 +1,110 @@
+#include <Servo.h>
+
+Servo servo1;
+Servo servo2;
+Servo servo3;
+
+int velmo = 5;// Tiempo (milisegundos)que tarda en dar un paso el motor paso a paso.
+int anges = 0;// Angulo escogido por el caracter seleccionado.
+int posicion_motor = 0;// grado en el que esta el motor paso a paso..
+
+void setup() {
+  
+ servo1.attach(3); 
+ servo2.attach(5);
+ servo3.attach(6);
+
+  servo1.write(127);
+  servo2.write(85);
+  servo3.write(35);
+  
+  pinMode(11, OUTPUT); // Pin 11 se conecta a al pin IN4 de la placa del motor paso a paso.
+  pinMode(10, OUTPUT); // Pin 10 se conecta a al pin IN3 de la placa del motor paso a paso.
+  pinMode(9, OUTPUT); // Pin 9 se conecta a al pin IN2 de la placa del motor paso a paso.
+  pinMode(8, OUTPUT); // Pin 8 se conecta a al pin IN1 de la placa del motor paso a paso.
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+
+int angulos[4][4]={{138,100,100,100,100,127},{63,91,95,90,87,85},{66,68,68,68,37,27},{0,0,0,95,95,0}};
+int i,j=0;
+    for(i=0;i<4;i++){
+       
+        servo1.write(angulos[0][i]);
+        delay(1200);
+        servo2.write(angulos[1][i]);
+        delay(1200);
+        servo3.write(angulos[2][i]);
+        delay(1200);
+         
+        
+         anges =angulos[3][i];
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+  
+    }
+}
+
+void paso_der(){// funcion_movimiento hacia la derecha del motor paso a paso.
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, HIGH);  
+   delay(velmo); 
+ digitalWrite(11, LOW); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, LOW);  
+   delay(velmo); 
+   digitalWrite(11, HIGH); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+     delay(velmo);  
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, HIGH);  
+     delay(velmo);   
+}
+
+void paso_izq() {//funcion_movimiento hacia la izquierda del motor paso a paso.
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+     delay(velmo);  
+ digitalWrite(11, LOW); 
+ digitalWrite(10, HIGH);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, LOW);  
+     delay(velmo);  
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, HIGH);  
+ digitalWrite(8, HIGH);  
+     delay(velmo);  
+ digitalWrite(11, HIGH); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, HIGH);  
+     delay(velmo); 
+}
+        
+void apagado() {// funcion_apagado del motor paso a paso.
+ digitalWrite(11, LOW); 
+ digitalWrite(10, LOW);  
+ digitalWrite(9, LOW);  
+ digitalWrite(8, LOW);  
+ }
